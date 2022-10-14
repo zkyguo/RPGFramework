1c92a1534293df544394b97262701b059fb3253a
Rui Guo <zkyguo123@gmail.com>
2022-10-14 00:30:24 -0400

    Add base Config setting system
    - Add Config Dictionary
    - Add get Config Method

--- Assets/RPGFramework/Scripts/2.System/0.Config/ConfigSetting.cs
+++ Assets/RPGFramework/Scripts/2.System/0.Config/ConfigSetting.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
+
+public enum ConfigType
+{
+    Equippement, Item, Weapon
+}
+
 /// <summary>
 /// Unique Config setting of game
 /// include all config setting
@@ -12,9 +18,34 @@ public class ConfigSetting : ConfigBase
 {
     /// <summary>
     /// Container of all config
+    /// < Type, <id, Config>>
     /// </summary>
-    [DictionaryDrawerSettings(KeyLabel ="Config Type")]
-    public Dictionary<string,Dictionary<int,ConfigBase>> CongfigDic = new Dictionary<string,Dictionary<int,ConfigBase>>();  
+    [DictionaryDrawerSettings(KeyLabel ="Config Type", ValueLabel ="List")]
+    public Dictionary<ConfigType, Dictionary<int,ConfigBase>> ConfigDic = new Dictionary<ConfigType, Dictionary<int,ConfigBase>>();
+
+    /// <summary>
+    /// Get config 
+    /// </summary>
+    /// <typeparam name="T">Type of config</typeparam>
+    /// <param name="configType">Name of Config</param>
+    /// <param name="id"> id of Config</param>
+    /// <returns></returns>
+    public T GetConfig<T>(ConfigType configType, int id) where T : ConfigBase
+    {
+        //Check if type exists
+        if(!ConfigDic.ContainsKey(configType))
+        {
+            throw new System.Exception($"Config {configType} : not found");
+          
+        }
+        //Check if id exists
+        if(!ConfigDic[configType].ContainsKey(id))
+        {
+            throw new System.Exception($"Config {configType} doesnt contain : {id}");
+        }
+
+        return ConfigDic[configType][id] as T;
 
 
+    }
 }
