89f1091233e811914e6fd6362049449766d296e7
Rui Guo <zkyguo123@gmail.com>
2022-10-14 23:10:39 -0400

    Add basic EditorOnLoad method

--- Assets/RPGFramework/Scripts/2.System/0.Config/GameSetting.cs
+++ Assets/RPGFramework/Scripts/2.System/0.Config/GameSetting.cs
@@ -1,9 +1,36 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
+
+/// <summary>
+/// Basic Game setting : 
+/// Default screen resolution, music, gamplay graphic, UI
+/// </summary>
 [CreateAssetMenu(fileName = "GameSetting", menuName = "RPGFramework/Config/GameSetting ")]
 public class GameSetting : ConfigBase
 {
-    
+#if UNITY_EDITOR
+
+
+    [Button(Name = "Initialize GameSetting", ButtonHeight = 50)]
+    [GUIColor(0,1,0)]
+    private void Init()
+    {
+        
+    }
+
+    /// <summary>
+    /// Execute while Editor is loading
+    /// </summary>
+    [InitializeOnLoadMethod]
+    private static void EditorOnLoad()
+    {
+        GameObject.Find("GameRoot").GetComponent<GameRoot>().GameSetting.Init();
+      
+    }
+
+#endif
 }
