8deb1fdd3eec3160240981d11dcf796d174dfa5f
Rui Guo <zkyguo123@gmail.com>
2022-11-03 23:42:14 -0400

    Add base Localisation system
    - Add base localisation setting instance
    - Add localisation data dic

---
+++ Assets/RPGFramework/Scripts/2.System/3.Localization/LocalizationSetting.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+/// <summary>
+/// Languages of game
+/// </summary>
+public enum LanguageType
+{
+    English = 0, Francais = 1
+}
+
+#region Localisation Language content
+/// <summary>
+/// Interface of content
+/// </summary>
+public interface ILanguangeContent
+{}
+
+/// <summary>
+/// Text language content
+/// </summary>
+[Serializable]
+public class L_Text : ILanguangeContent
+{
+    public string content;
+}
+
+[Serializable]
+public class L_Image : ILanguangeContent
+{
+    public Image content;
+}
+
+/// <summary>
+/// Audio localisation content
+/// </summary>
+[Serializable]
+public class L_Audio : ILanguangeContent
+{
+    public AudioClip content;
+}
+
+/// <summary>
+/// Audio localisation content
+/// </summary>
+[Serializable]
+public class L_Video : ILanguangeContent
+{
+    public VideoClip content;
+}
+#endregion
+
+/// <summary>
+/// Language model, which define different language possible on the same object
+/// </summary>
+public class LocalizationModel
+{
+    [DictionaryDrawerSettings(DisplayMode = DictionaryDisplayOptions.OneLine, KeyLabel = "Language", ValueLabel = "Value")]
+    public Dictionary<LanguageType, ILanguangeContent> ContentDic = new Dictionary<LanguageType, ILanguangeContent>()
+    {
+        {LanguageType.English, new L_Text()},
+        {LanguageType.Francais, new L_Text()}
+    };
+
+
+}
+
+[CreateAssetMenu(fileName ="Localization", menuName ="RPGFramework/LocalizationConfig")]
+public class LocalizationSetting : ConfigBase
+{
+    /// <summary>
+    /// <PackageName,<contentKey, languageValue>>
+    /// </summary>
+    [SerializeField]
+    [DictionaryDrawerSettings(DisplayMode = DictionaryDisplayOptions.OneLine, KeyLabel ="Data package", ValueLabel ="Language Data")]
+    private Dictionary<string, Dictionary<string, LocalizationModel>> DataPackage;
+
+    public T GetContent<T>(string packageName, string contentKey, LanguageType languageType) where T : class, ILanguangeContent
+    {
+        return DataPackage[packageName][contentKey].ContentDic[languageType] as T;
+    }
+}
